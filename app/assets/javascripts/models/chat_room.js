d1b6788c66dd427fdeb9e75ccaeb1b750f66b6aa
Brian Heithaus <bheithaus@gmail.com>
2013-04-26 18:31:53 -0700

    added BB message model and collection

--- app/assets/javascripts/models/chat_room.js
+++ app/assets/javascripts/models/chat_room.js
@@ -1,4 +1,10 @@
 CH.Models.ChatRoom = Backbone.RelationalModel.extend({
 	urlRoot: "/chatrooms"
-	
+	relations: [{
+		type: "Backbone.HasMany",
+		key: "messages",
+		relatedModel: "CH.Models.Message",
+		collectionType: "CH.Models.Messages",
+		collectionOptions: ""
+	}]
 })
